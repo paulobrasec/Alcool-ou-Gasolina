19cd95867b1ab0a504ea9f3c6fd9661fa8e1e50c
Paulo Santos <paulobrasec@gmail.com>
2018-09-08 03:07:19 -0300

    Init
    Interface

---
+++ Alcool ou Gasolina/Alcool ou Gasolina/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alcool ou Gasolina
+//
+//  Created by Paulo Cesar on 08/09/2018.
+//  Copyright © 2018 Paulo Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
